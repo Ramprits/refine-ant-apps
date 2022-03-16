f648342f7dad0b6750f92d2041bdeb0174798920
Ramprit Sahani <21260321+Ramprits@users.noreply.github.com>
2022-03-16 10:42:40 +0530

    Initial commit

---
+++ src/pages/SignUp.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import { Layout, Button, Card, Form, Input, Checkbox } from "antd";
+
+import { Link } from "react-router-dom";
+
+const { Content } = Layout;
+export default class SignUp extends Component {
+  render() {
+    const onFinish = (values) => {
+      console.log("Success:", values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+      console.log("Failed:", errorInfo);
+    };
+    return (
+      <>
+        <div className="layout-default ant-layout layout-sign-up">
+          <Content className="mt-15">
+            <Card
+              className="card-signup header-solid h-full ant-card pt-0"
+              title={<h5>Register With</h5>}
+              bordered="false"
+            >
+              <Form
+                name="basic"
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                className="row-col"
+              >
+                <Form.Item
+                  name="Name"
+                  rules={[
+                    { required: true, message: "Please input your username!" }
+                  ]}
+                >
+                  <Input placeholder="Name" />
+                </Form.Item>
+                <Form.Item
+                  name="email"
+                  rules={[
+                    { required: true, message: "Please input your email!" }
+                  ]}
+                >
+                  <Input placeholder="email" />
+                </Form.Item>
+                <Form.Item
+                  name="password"
+                  rules={[
+                    { required: true, message: "Please input your password!" }
+                  ]}
+                >
+                  <Input placeholder="Passwoed" />
+                </Form.Item>
+
+                <Form.Item name="remember" valuePropName="checked">
+                  <Checkbox>
+                    I agree the{" "}
+                    <a href="#pablo" className="font-bold text-dark">
+                      Terms and Conditions
+                    </a>
+                  </Checkbox>
+                </Form.Item>
+
+                <Form.Item>
+                  <Button
+                    style={{ width: "100%" }}
+                    type="primary"
+                    htmlType="submit"
+                  >
+                    SIGN UP
+                  </Button>
+                </Form.Item>
+              </Form>
+              <p className="font-semibold text-muted text-center">
+                Already have an account?{" "}
+                <Link to="/sign-in" className="font-bold text-dark">
+                  Sign In
+                </Link>
+              </p>
+            </Card>
+          </Content>
+        </div>
+      </>
+    );
+  }
+}
